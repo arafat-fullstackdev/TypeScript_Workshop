9a6ea9facf38ca6341b1229e9f9412422a5303b0
Arafat Patwary <farabipsk@gmail.com>
2025-03-19 19:26:06 +0600

    Use basic ts

--- Basic/script.ts
+++ Basic/script.ts
@@ -6,3 +6,24 @@ const result = add(22 ,44);
 
 
 console.log(result);
+console.log('TS start!');
+console.log('TS start!');
+
+//TS inference 
+
+let playerName: string;
+playerName = 'Paskob';
+console.log(playerName);
+
+let footboller;  // Declare
+console.log(footboller);
+
+footboller = 'Musa';  // Assign
+console.log(footboller);
+
+//?Function
+function Player(a:number,b:number):number{
+    return a * b;
+}
+
+console.log(Player(11,5));
